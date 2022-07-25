cf14dc324caad8d736b3b92eeffd15d1a2684a09
Cinny <cinnymaria@gmail.com>
2022-07-25 15:02:16 +0200

    set-up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import'./Images/image.png';
 
 
 function App() {
 	return (
 		<div>
 			<div className='img-header'>
-				<img src='./image.png' alt='image' />
+				<img src='./' alt='image' />
 			</div>
 			<div className='title'>
 				<h1>Declaration App</h1>
